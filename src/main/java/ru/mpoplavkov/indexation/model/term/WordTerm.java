49ce5ff3ebcca7c1b477d919624efd703d94daa4
mpoplavkov <mihail.poplavkov@gmail.com>
2020-12-10 17:02:04 +0100

    Word preconditions

--- src/main/java/ru/mpoplavkov/indexation/model/term/WordTerm.java
+++ src/main/java/ru/mpoplavkov/indexation/model/term/WordTerm.java
@@ -1,10 +1,18 @@
 package ru.mpoplavkov.indexation.model.term;
 
+import com.google.common.base.Preconditions;
 import lombok.Data;
 
 @Data
 public class WordTerm implements Term {
 
     private final String word;
+
+    public WordTerm(String word) {
+        Preconditions.checkArgument(!word.isEmpty(), "Word should not be empty");
+        Preconditions.checkArgument(!word.contains("\\s"), "Word should not contain whitespace symbols");
+
+        this.word = word;
+    }
 
 }
