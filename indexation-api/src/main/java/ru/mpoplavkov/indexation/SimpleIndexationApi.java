624bf231aaf5bc340fbb669eac04cc5af2dfc2f6
mpoplavkov <mihail.poplavkov@gmail.com>
2020-12-15 16:46:13 +0100

    Add spring

---
+++ indexation-api/src/main/java/ru/mpoplavkov/indexation/SimpleIndexationApi.java
@@ -0,0 +1,13 @@
+package ru.mpoplavkov.indexation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleIndexationApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleIndexationApi.class, args);
+    }
+
+}
