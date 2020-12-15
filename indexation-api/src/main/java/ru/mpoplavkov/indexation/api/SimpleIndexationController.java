fcc02a232c1b477aa47cae013fd8711c5a93e17c
mpoplavkov <mihail.poplavkov@gmail.com>
2020-12-15 18:15:00 +0100

    Controller exception handlers

--- indexation-api/src/main/java/ru/mpoplavkov/indexation/api/SimpleIndexationController.java
+++ indexation-api/src/main/java/ru/mpoplavkov/indexation/api/SimpleIndexationController.java
@@ -1,22 +1,33 @@
 package ru.mpoplavkov.indexation.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.mpoplavkov.indexation.model.query.ExactTerm;
 import ru.mpoplavkov.indexation.model.query.Query;
 import ru.mpoplavkov.indexation.model.term.WordTerm;
 import ru.mpoplavkov.indexation.service.FileSystemIndexService;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Set;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 @RestController
 public class SimpleIndexationController {
+
+    @ExceptionHandler({FileNotFoundException.class})
+    public ResponseEntity<String> handleFileNotFoundException(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler({IOException.class})
+    public ResponseEntity<String> handleIOException(Exception e) {
+        return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
+    }
 
     @Autowired
     private FileSystemIndexService fileSystemIndexService;
